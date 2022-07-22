dda0ade372b28a2ff5e8bc6b8981704c35799e76
Powsikan <powsi1997@gmail.com>
2022-07-22 18:40:18 +0530

    chart issue fixed

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,63 +3,52 @@ import { render } from "react-dom";
 // Import Highcharts
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import { Card } from '@mui/material';
+import { Card } from "@mui/material";
 
-export default function Chart({data}){
-console.log("afrin",data)
-console.log("afrin-purch",data.purchaseDate)
-  let purchaseDate = []
-  let totalSales = []
+export default function Chart({ purchaseDate, totalSales }) {
+  const chartOptions = {
+    title: {
+      text: "Shopify Sales",
+    },
+    chart: {
+      type: "column",
+    },
+    xAxis: {
+      categories: purchaseDate !== undefined ? purchaseDate : [], //['2022-06-16','2022-06-18','2022-06-29','2022-06-29','2022-07-08','2022-07-17']
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+      },
+    },
+    series: [
+      {
+        name: "Shopify",
+        // data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        data: totalSales !== undefined ? totalSales : [], //[37.8,34.16,41.99,41.82,107.99,293],
+        color: "#64943E",
+      },
+    ],
+    credits: {
+      enabled: false,
+    }, // The water mark removal place
+  };
 
-  useEffect(()=>{
-    purchaseDate = data.map(d=>d.purchaseDate)
-    totalSales = data.map(d=>d.totalSalesAmount)
-  },[data])
-
-const chartOptions = {
-  title: {
-    text: "Shopify Demo"
-  },
-  chart: {
-    type: 'column'
-},
-xAxis:{
-  categories:purchaseDate!==undefined ?purchaseDate:[]//['2022-06-16','2022-06-18','2022-06-29','2022-06-29','2022-07-08','2022-07-17']
-},
-plotOptions: {
-  column: {
-      pointPadding: 0.2,
-      borderWidth: 0
-  }
-},
-  series: [{
-    name: 'Shopify',
-    // data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-    data:totalSales !==undefined? totalSales:[],//[37.8,34.16,41.99,41.82,107.99,293],
-    color:'#64943E'
-
-}, 
-
-
-
-
-
-],
-credits:{
-  enabled:false
-}// The water mark removal place
-
-};
-      
-  return(
+  return (
     <>
-    <div>
-      <Card sx={{width:"600px",height:"400px", marginLeft:"0px", alignContent:"center"}}>
-      <HighchartsReact options ={chartOptions} highcharts={Highcharts}/>
-      </Card>
-    </div>
-    
+      <div>
+        <Card
+          sx={{
+            width: "600px",
+            height: "400px",
+            marginLeft: "0px",
+            alignContent: "center",
+          }}
+        >
+          <HighchartsReact options={chartOptions} highcharts={Highcharts} />
+        </Card>
+      </div>
     </>
-  )
+  );
 }
-
--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,88 +1,102 @@
-import React, {useEffect, useState} from 'react';
-import {makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
-import {Dashboard} from '../model/Dashboard';
+import React, { useEffect, useState } from "react";
+import {
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+import { Dashboard } from "../model/Dashboard";
 
-import {gql, useQuery} from "@apollo/client";
-import Chart from './Chart';
+import { gql, useQuery } from "@apollo/client";
+import Chart from "./Chart";
 
 const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
+  table: {
+    minWidth: 650,
+  },
 });
 
-
 const DashboardPage = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const QUERY = gql`
-        {
-            findByOrganizationId(organizationId: 1) {
-                purchaseDate
-                platformStoreId
-                platformDataType
-                totalSalesAmount
-                totalDiscountAmount
-                totalTaxAmount
-                currency
-                totalOrderItemsQty
-            }
-        }
-    `;
+  const QUERY = gql`
+    {
+      findByOrganizationId(organizationId: 1) {
+        purchaseDate
+        platformStoreId
+        platformDataType
+        totalSalesAmount
+        totalDiscountAmount
+        totalTaxAmount
+        currency
+        totalOrderItemsQty
+      }
+    }
+  `;
 
-    const {data} = useQuery(QUERY);
+  const { data } = useQuery(QUERY);
 
-    useEffect(() => {
-        setDashboardData(data?.findByOrganizationId)
-    }, [data]);
+  const [dashboardData, setDashboardData] = useState<Dashboard[]>([]);
+  const [purchaseDate, setPurchaseDate] = useState<any[]>([]);
+  const [totalSales, setTotalSales] = useState<any[]>([]);
+  const [showTable, setShowTable] = useState<any>(false);
 
-
-    const [dashboardData, setDashboardData] = useState<Dashboard[]>([]);
-
-    return (
-        <div className={"formContainer"}>
-            <div className={"textHead"}>Shopify-Demo</div>
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Purchase Date</TableCell>
-                            <TableCell align="right">Platform Store Id</TableCell>
-                            <TableCell align="right">Platform Data Type</TableCell>
-                            <TableCell align="right">Total Sales Amount</TableCell>
-                            <TableCell align="right">Total Discount Amount</TableCell>
-                            <TableCell align="right">Total Tax Amount</TableCell>
-                            <TableCell align="right">Currency</TableCell>
-                            <TableCell align="right">Total Order Items Qty</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {dashboardData?.map((row) => (
-                            <TableRow key={row.purchaseDate}>
-                                <TableCell component="th" scope="row">
-                                    {row.purchaseDate}
-                                </TableCell>
-                                <TableCell align="right">{row.platformStoreId}</TableCell>
-                                <TableCell align="right">{row.platformDataType}</TableCell>
-                                <TableCell align="right">{row.totalSalesAmount}</TableCell>
-                                <TableCell align="right">{row.totalDiscountAmount}</TableCell>
-                                <TableCell align="right">{row.totalTaxAmount}</TableCell>
-                                <TableCell align="right">{row.currency}</TableCell>
-                                <TableCell align="right">{row.totalOrderItemsQty}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-                
-                <br></br>
-                <Chart data ={dashboardData}/>
-            
-
-        </div>
+  useEffect(() => {
+    setDashboardData(data?.findByOrganizationId);
+    setPurchaseDate(data?.findByOrganizationId.map((d: any) => d.purchaseDate));
+    setTotalSales(
+      data?.findByOrganizationId.map((d: any) => d.totalSalesAmount)
     );
+  }, [data]);
+
+  return (
+    <div className={"formContainer"}>
+      <div className={"textHead"}>Shopify-Demo</div>
+      <br></br>
+      <div className="chart-div" onClick={() => setShowTable(!showTable)}>
+        <Chart purchaseDate={purchaseDate} totalSales={totalSales} />
+      </div>
+      <br></br>
+      {showTable && (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Purchase Date</TableCell>
+                <TableCell align="right">Platform Store Id</TableCell>
+                <TableCell align="right">Platform Data Type</TableCell>
+                <TableCell align="right">Total Sales Amount</TableCell>
+                <TableCell align="right">Total Discount Amount</TableCell>
+                <TableCell align="right">Total Tax Amount</TableCell>
+                <TableCell align="right">Currency</TableCell>
+                <TableCell align="right">Total Order Items Qty</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dashboardData?.map((row) => (
+                <TableRow key={row.purchaseDate}>
+                  <TableCell component="th" scope="row">
+                    {row.purchaseDate}
+                  </TableCell>
+                  <TableCell align="right">{row.platformStoreId}</TableCell>
+                  <TableCell align="right">{row.platformDataType}</TableCell>
+                  <TableCell align="right">{row.totalSalesAmount}</TableCell>
+                  <TableCell align="right">{row.totalDiscountAmount}</TableCell>
+                  <TableCell align="right">{row.totalTaxAmount}</TableCell>
+                  <TableCell align="right">{row.currency}</TableCell>
+                  <TableCell align="right">{row.totalOrderItemsQty}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
-
-
